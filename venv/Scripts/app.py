45f4a5d308898388cde0ccf3016f55231d9d99a5
Help <pratikgangan19@gmail.com>
2020-04-21 17:22:55 +0530

    final submission

--- venv/Scripts/app.py
+++ venv/Scripts/app.py
@@ -1,6 +1,6 @@
 
-from flask import Flask
-from connection import movieMetadata
+from flask import Flask, request
+from Movies import movieMetadata
 
 app = Flask(__name__)
 
@@ -9,9 +9,13 @@ def index():
   return 'Server Works!'
   
 
-
-@app.route('/api/movie/<string:name>', methods=['GET', 'POST']) 
+@app.route('/api/movie/<string:name>') 
 def api(name):
-    merged=movieMetadata(name)
-    return merged
+   merged=movieMetadata(name)
+   return merged
 
+@app.route('/api/movie/') 
+def search():
+    search_value= request.args.get('search_field')
+    
+    return '<h1> it is {}</h1>'.format(search_value)
